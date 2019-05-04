92610cf12fe57220e3e34b7f32be6b07bb84fcf3
Roberto Cai <robertocw_18@hotmail.com>
2019-05-04 10:43:38 +0200

    move index.js to hmtl

--- scripts/index.js
+++ scripts/index.js
@@ -1,52 +1,24 @@
 $(document).ready(function() {
 
     // var roomName = localStorage.getItem("room_name");
-    if (localStorage.getItem("room_name")) {
-        roomName = localStorage.getItem("room_name");
-        // localStorage.clear();
-    }
-    var domain = "meet.jit.si"
-    var options = {
-        roomName: roomName,
-        // width: 800,
-        // heigth: 600,
-        parentNode: document.querySelector('#jitsi_video')
-    }
-    var api = new JitsiMeetExternalAPI(domain, options);
+    // if (localStorage.getItem("speech")) {
+    //     speech = localStorage.getItem("speech");
+    //     // localStorage.clear();
+    // }
 
-});
-
-var keys = {};
-var keysOfInterest = [37, 38, 39, 40];
-$(document).keydown(function (e) {
-    keys[e.which] = true;
-    console.log(keys)
-
-    publishKeys();
 
 });
 
 function recognized_speech() {
     recognized_speech = "";
 
-    var pressedKeysMsg = new ROSLIB.Message({
+    var speech = new ROSLIB.Message({
                         data : recognized_speech
                        });
-
-}
-
-function printKeys() {
-    var html = '';
-    for (var i in keys) {
-        if (!keys.hasOwnProperty(i)) continue;
-        html += '<p>' + i + '</p>';
-
-    }
-    $('#key_press').html(html);
 }
 
 var ros = new ROSLIB.Ros({
-    url: "ws://localhost:9090"
+    url: "ws://10.18.1.10:9090"
 })
 
 ros.on("connection", function() {
@@ -68,9 +40,9 @@ var listener_subscriber = new ROSLIB.Topic({
 });
 
 listener_subscriber.subscribe(function(message) {
-    console.log('Received message on ' + listener.name + ': ' + message.data);
+    console.log('Received message on ' + listener_subscriber.name + ': ' + message.data);
     localStorage.setItem("speech", message.data);
     location.reload();
-    var go_2019_speech_recognizer = document.querySelector("#go_2019_speech_recognizer")
+    var go_2019_speech_recognizer = document.getElementById("#go_2019_speech_recognizer")
     go_2019_speech_recognizer.value = message.data
 });
